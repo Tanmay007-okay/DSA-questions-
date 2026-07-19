f6609aaab3fb84c539bc498bbd81a5e4ce91f3dd
Tanmay Joshi <tanmayjoshi347@gmail.com>
2026-07-19 21:33:29 +0530

    Time: 2 ms (40.09%) | Memory: 10.5 MB (68.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void find(int ind,vector<int>& nums,vector<int>& ds,set<vector<int>>& result){ //using seet to tackle duplicates
-        if(ind==nums.size()){
-            result.insert(ds); // no push_back in set we use insert
-            return;
+    void find(int ind,vector<int>& nums,vector<int>& current,vector<vector<int>>& result){
+        result.push_back(current);
+
+        for(int i=ind;i<nums.size();i++){
+            if(i>ind && nums[i] == nums[i-1]) continue;
+
+            current.push_back(nums[i]);
+            
+            find(i+1,nums,current,result);
+            current.pop_back();
         }
-        ds.push_back(nums[ind]);
-        // to pick the current
-        find(ind+1,nums,ds,result);
-        // not pick
-        ds.pop_back();
-        find(ind+1,nums,ds,result);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> result;
-        vector<int> ds;
+        vector<vector<int>> result;
+        vector<int> current;
         sort(nums.begin(),nums.end());
 
-        find(0,nums,ds,result);
+        find(0,nums,current,result);
 
-        vector<vector<int>> ans(result.begin(), result.end());
-        return ans;
+        
+        return result;
 
 
 
