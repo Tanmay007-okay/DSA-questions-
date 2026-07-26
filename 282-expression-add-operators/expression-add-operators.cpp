297f16daa4fa0001b07ab5f01fb598f9e704a2e5
Tanmay Joshi <tanmayjoshi347@gmail.com>
2026-07-26 21:39:57 +0530

    Time: 403 ms (61.05%) | Memory: 93.5 MB (28.07%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+     void dfs(string& num, int target, int start, long long current_value, long long last_operand, string expression, vector<string>& result) {
+        
+        if (start == num.size()) {
+            
+            if (current_value == target)  
+                result.push_back(expression);
+            return;
+        }
+        
+     
+        for (int i = start; i < num.size(); i++) {
+            // Skip leading zeros in numbers
+            if (i > start && num[start] == '0') return;
+            // Get the current number
+            string current_num = num.substr(start, i - start + 1); 
+            long long current_num_val = stoll(current_num);
+            
+            // If we are at the first number, just start the expression
+            if (start == 0) {
+                dfs(num, target, i + 1, current_num_val, current_num_val, current_num, result);
+            } else {
+                // Add the current number with '+'
+                dfs(num, target, i + 1, current_value + current_num_val, current_num_val, expression + "+" + current_num, result);
+                
+                // Add the current number with '-'
+                dfs(num, target, i + 1, current_value - current_num_val, -current_num_val, expression + "-" + current_num, result);
+                
+                // Add the current number with '*'
+                dfs(num, target, i + 1, current_value - last_operand + last_operand * current_num_val, last_operand * current_num_val, expression + "*" + current_num, result);
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+     vector<string> result;  
+        // Start DFS with empty expression
+        dfs(num, target, 0, 0, 0, "", result);  
+        return result;   
+    }
+};
