383b613a83d053fba788d0dc769ebeeac3c64e94
Tanmay Joshi <tanmayjoshi347@gmail.com>
2026-08-22 16:50:19 +0530

    Time: 4 ms (59.04%) | Memory: 28.5 MB (39.78%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -5,13 +5,15 @@ public:
         vector<int> nge(n);
         stack<int> st;
         for(int i=2*n-1;i>=0;i--){
-            while(!st.empty() && st.top()<=nums[i%n]){
+            int ind=i%n;
+            int currEle=nums[ind];
+            while(!st.empty() && st.top()<=currEle){
                 st.pop();
             }
             if(i<n){
                 nge[i]=st.empty() ?  -1:st.top();
             }
-            st.push(nums[i%n]);
+            st.push(currEle);
         }
         return nge;
     }
