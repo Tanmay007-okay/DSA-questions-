40fec0c9eac883025a78c3d3e0a77d706c7e32db
Tanmay Joshi <tanmayjoshi347@gmail.com>
2026-08-24 16:48:47 +0530

    Time: 19 ms (13.27%) | Memory: 14 MB (80.77%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n;i++){
             int smallest=nums[i];
             int largest=nums[i];
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 smallest=min(smallest,nums[j]);
                 largest=max(largest,nums[j]);
                 sum += (largest-smallest);
