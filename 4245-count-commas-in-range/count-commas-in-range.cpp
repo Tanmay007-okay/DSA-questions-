2323c6dea98fdf843733167180485af0b9ff7e8d
Tanmay Joshi <tanmayjoshi347@gmail.com>
2026-09-08 23:07:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.51%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(int n) {
+        int ans = 0;
+        if (n >= 1000)ans += n - 999;
+
+        if (n >= 1000000)ans += n - 999999;
+
+        if (n >= 1000000000)ans += n - 999999999;
+
+        return ans;
+    }
+};
